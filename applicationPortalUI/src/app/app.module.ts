69041da3b055f056c0b114f5bb2580569fdbf4c8
Egiantsgit <31530497+Egiantsgit@users.noreply.github.com>
2018-01-23 20:54:54 -0500

    Add files via upload
    First Commit

---
+++ applicationPortalUI/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
